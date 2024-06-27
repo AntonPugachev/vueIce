75d36b5fd5f535a839667fd6eb6b520b02de074b
Anton Pugachev <anton.pugachev@bluescape.com>
2024-06-27 15:00:17 -0400

    fix: [main] fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,5 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', '@vue/eslint-config-typescript', '@vue/eslint-config-prettier', 'plugin:storybook/recommended'],
 };
